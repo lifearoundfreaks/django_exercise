5ae67ea9ed949a652a20a16e7ed28b731a65671c
Rustam <rystamrv@gmail.com>
2019-05-18 18:27:19 +0300

    Update views.py

--- employees/views.py
+++ employees/views.py
@@ -1,17 +1,57 @@
 from django.shortcuts import render
 from .models import Employees
-from .models import Department
-from django.core.paginator import Paginator
 from django.db.models import Q
 
 
+def employees_sort_fullname(request):
+
+    obj_list = Employees.objects.all().order_by("fullname")
+
+    content = {
+        "obj_list": obj_list,
+        "title": "List"
+    }
+    return render(request, "employees/index.html", content)
+
+
+def employees_sort_position(request):
+    obj_list = Employees.objects.all().order_by("position")
+
+    content = {
+        "obj_list": obj_list,
+        "title": "List"
+    }
+    return render(request, "employees/index.html", content)
+
+
+def employees_sort_department(request):
+    obj_list = Employees.objects.all().order_by("department")
+
+    content = {
+        "obj_list": obj_list,
+        "title": "List"
+    }
+    return render(request, "employees/index.html", content)
+
+
+def employees_sort_salary(request):
+    obj_list = Employees.objects.all().order_by("salary")
+
+    content = {
+        "obj_list": obj_list,
+        "title": "List"
+    }
+    return render(request, "employees/index.html", content)
+
+
 def employees_list(request):
+
     search = request.GET.get('search', '')
 
     if search:
         obj_list = Employees.objects.filter(Q(fullname__icontains=search))
     else:
-        obj_list = Employees.objects.all().order_by("department")
+        obj_list = Employees.objects.all().order_by("fullname")
 
     content = {
         "obj_list": obj_list,
