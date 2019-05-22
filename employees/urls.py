6c5eff1203bce765799137cbd793068e7e2986b6
Rustam <rystamrv@gmail.com>
2019-05-22 09:51:48 +0300

    Update urls.py

--- employees/urls.py
+++ employees/urls.py
@@ -4,7 +4,8 @@ from .views import *
 
 urlpatterns = [
     path(r'', employees_list),
-    path(r'sort=fullname', employees_sort_fullname),
+    path(r'search_fullname/$', search_fullname, name='search_fullname'),
+    path(r'employees_sort_fullname', employees_sort_fullname, name='employees_sort_fullname'),
     path(r'sort=position', employees_sort_position),
     path(r'sort=department', employees_sort_department),
     path(r'sort=salary', employees_sort_salary),
