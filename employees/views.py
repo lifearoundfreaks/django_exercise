f9e73472138e144e23741ae7c6f53ea1b5791f6d
Rustam <rystamrv@gmail.com>
2019-05-22 09:52:23 +0300

    Update views.py

--- employees/views.py
+++ employees/views.py
@@ -5,7 +5,7 @@ from django.db.models import Q
 
 def employees_sort_fullname(request):
 
-    obj_list = Employees.objects.all().order_by("fullname")
+    obj_list = Employees.objects.all().order_by("salary")
 
     content = {
         "obj_list": obj_list,
@@ -48,6 +48,22 @@ def employees_list(request):
 
     search = request.GET.get('search', '')
 
+    if search:
+        obj_list = Employees.objects.filter(Q(fullname__icontains=search))
+    else:
+        obj_list = Employees.objects.all().order_by("fullname")
+
+    content = {
+        "obj_list": obj_list,
+        "title": "List"
+    }
+    return render(request, "employees/index.html", content)
+
+
+def search_fullname(request):
+
+    search = request.GET.get('search', '')
+    print("ff")
     if search:
         obj_list = Employees.objects.filter(Q(fullname__icontains=search))
     else:
