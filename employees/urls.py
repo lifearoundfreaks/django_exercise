9b6135e9de07ad1d27798ad378278d9b46faeb81
Rustam <rystamrv@gmail.com>
2019-05-25 14:57:08 +0300

    Update urls.py

--- employees/urls.py
+++ employees/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from .views import *
+
 
 urlpatterns = [
     path(r'', employees_list),
+    path(r'sort_fullname/', SortFullName.as_view()),
+    path(r'sort_position/', SortPosition.as_view()),
+    path(r'sort_salary/', SortSalary.as_view()),
+    path(r'sort_department/', SortDepartment.as_view()),
+    path(r'<int:id>/', employees_detail),
     path(r'search_fullname/$', search_fullname, name='search_fullname'),
-    path(r'employees_sort_fullname', employees_sort_fullname, name='employees_sort_fullname'),
-    path(r'sort=position', employees_sort_position),
-    path(r'sort=department', employees_sort_department),
-    path(r'sort=salary', employees_sort_salary),
-    path(r'<int:id>/', employees_detail)
 ]
