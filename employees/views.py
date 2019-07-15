de6bca09071f0ca388db8504be67649bda6e318e
lifearoundfreaks <42870020+lifearoundfreaks@users.noreply.github.com>
2019-07-15 09:41:21 +0300

    some tweaks and more comments

--- employees/views.py
+++ employees/views.py
@@ -4,7 +4,7 @@ from .utilities import PaginationSetter
 from django.db.models import Q
 from django.core.exceptions import FieldDoesNotExist
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.contrib.admin.views.decorators import staff_member_required
 from .forms import EmployeeForm
 from .decorators import user_access_error
@@ -47,47 +47,22 @@ def employee(request, employee_id):
         "form": EmployeeForm,
     }
 
-    if request.method == 'GET':
-        return render(request, "employees/personal.html", context)
-
-    if request.method == 'POST':
-        instance = get_object_or_404(Employee, id=employee_id)
-        form = EmployeeForm(
-            request.POST, request.FILES, instance=instance)
-
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/')
-
-        return render(request, 'employees/personal.html', context)
+    # Render appropriate employee's personal page
+    return render(request, "employees/personal.html", context)
 
 
 @user_access_error
 @staff_member_required(login_url='login')
 def search(request):
     if request.method == 'GET':
-        try:
-            # Current page parameter. 1 by default
-            page = max(int(request.GET.get('page')), 1)
+        # Current page parameter. 1 by default
+        page = int(request.GET.get('page') or 1)
 
-        except (ValueError, TypeError):
-            page = 1
+        # Page size parameter. 10 by default
+        page_size = int(request.GET.get('page_size') or 10)
 
-        try:
-            # Page size parameter. 10 by default
-            page_size = max(int(request.GET.get('page_size')), 1)
-
-        except (ValueError, TypeError):
-            page_size = 10
-
-        try:
-            # Query by which to search a database. Use whitespaces for
-            # multiple words
-            query = request.GET.get('query')
-            if not query:
-                raise ValueError
-        except ValueError:
-            query = ""
+        # Search query. Empty string by default
+        query = request.GET.get('query') or ""
 
         # First, let's get all objects
         employees = Employee.objects.all()
@@ -114,7 +89,7 @@ def search(request):
             employees = employees.order_by('last_name')[
                 (page - 1) * page_size:page * page_size]
 
-        # Different logic if this view was called by ajax
+        # Branching logic if this view was called by ajax
         if request.GET.get('ajax'):
             context = {
                 "employees": employees,
@@ -145,58 +120,89 @@ def search(request):
 @user_access_error
 @staff_member_required(login_url='login')
 def create_employee(request):
+    # Form with employee data or no data
     form = EmployeeForm(request.POST or None)
+    # If form is correctly filled
     if form.is_valid():
+        # Save employee
         form.save()
+        # Redirect to the main page
         return redirect("/")
+
     context = {
         "form": form,
         "title": "Employee creation",
         "title_id": "add-employee",
     }
+
+    # Else, render template the same form
     return render(request, "employees/create.html", context)
 
 
 @user_access_error
 @staff_member_required(login_url='login')
 def edit_employee(request, employee_id):
+    # Get employee
     employee = get_object_or_404(Employee, id=employee_id)
+    # Get employee form (with filled in fields)
     form = EmployeeForm(
         request.POST or None, request.FILES or None, instance=employee)
+
+    # When form data was valid
     if form.is_valid():
+        # Save employee
         form.save()
+        # Redirect to their page
         return redirect(f"/employees/{employee_id}")
+
     context = {
         "employee": employee,
         "form": form,
         "title": "Updating employee",
     }
+
+    # If not, render edit template
     return render(request, "employees/edit.html", context)
 
 
 @user_access_error
 @staff_member_required(login_url='login')
 def delete_employee(request, employee_id):
+    # Get employee
     e = get_object_or_404(Employee, id=employee_id)
+    # POST means that user confirmed deletion
     if request.method == "POST":
+        # Delete user
         e.delete()
+        # Redirect to the main page
         return redirect("/")
+
     context = {
         "employee": e,
         "title": "Employee deletion",
     }
+
+    # While not POST, ask for user confirmation via this template
     return render(request, "employees/delete.html", context)
 
 
 @staff_member_required(login_url='login')
 def reassign_boss(request):
     if request.method == 'POST':
+        # If it's a proper request
         if 'boss_id' in request.POST and 'sub_id' in request.POST:
+            # Find a boss
             boss = Employee.objects.get(id=request.POST['boss_id'])
+            # Find a subordinate
             sub = Employee.objects.get(id=request.POST['sub_id'])
+            # If boss is in appropriate position to subordinate
             if boss in get_appropriate_bosses(sub.position, sub.dept):
+                # Make him subordinate's boss
                 sub.boss = boss
+                # Save subordinate
                 sub.save()
+                # And report that operation was successful
                 return HttpResponse(f"Successfully reassigned.")
             else:
+                # Else report failure
                 return HttpResponse(f"Boss position was inappropriate.")
